6d6fae72a11d739c64b2c825418c285385f75ade
Matt Massie <massie@cs.berkeley.edu>
2013-04-11 14:14:33 -0700

    Initial commit

---
+++ src/main/java/edu/berkeley/amplab/bamavro/Main.java
@@ -0,0 +1,11 @@
+package edu.berkeley.amplab.bamavro;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: massie
+ * Date: 4/11/13
+ * Time: 2:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
