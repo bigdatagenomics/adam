25d4df1d8f29db7cfef8cbf254a2ed176caeb5d8
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2014-01-28 18:23:07 -0800

    Added ability to create regions from reads, and to merge adjacent regions.

--- adam-core/src/test/scala/edu/berkeley/cs/amplab/adam/models/ReferenceRegionSuite.scala
+++ adam-core/src/test/scala/edu/berkeley/cs/amplab/adam/models/ReferenceRegionSuite.scala
@@ -16,6 +16,7 @@
 package edu.berkeley.cs.amplab.adam.models
 
 import org.scalatest._
+import edu.berkeley.cs.amplab.adam.avro.ADAMRecord
 
 class ReferenceRegionSuite extends FunSuite {
 
@@ -35,9 +36,9 @@ class ReferenceRegionSuite extends FunSuite {
     assert(!region(0, 10, 100).contains(point(1, 50)))
   }
 
-  test("union") {
+  test("merge") {
     intercept[AssertionError] {
-      region(0, 10, 100).union(region(1, 10, 100))
+      region(0, 10, 100).merge(region(1, 10, 100))
     }
 
     val r1 = region(0, 10, 100)
@@ -47,9 +48,9 @@ class ReferenceRegionSuite extends FunSuite {
     val r12 = region(0, 0, 100)
     val r13 = region(0, 10, 150)
 
-    assert(r1.union(r1) === r1)
-    assert(r1.union(r2) === r12)
-    assert(r1.union(r3) === r13)
+    assert(r1.merge(r1) === r1)
+    assert(r1.merge(r2) === r12)
+    assert(r1.merge(r3) === r13)
   }
 
   test("overlaps") {
@@ -78,22 +79,22 @@ class ReferenceRegionSuite extends FunSuite {
   test("distance(: ReferenceRegion)") {
 
     // distance on the right
-    assert(region(0, 10, 100).distance(region(0, 200, 300)) === Some(100))
+    assert(region(0, 10, 100).distance(region(0, 200, 300)) === Some(101))
 
     // distance on the left
-    assert(region(0, 100, 200).distance(region(0, 10, 50)) === Some(50))
+    assert(region(0, 100, 200).distance(region(0, 10, 50)) === Some(51))
 
     // different sequences
     assert(region(0, 100, 200).distance(region(1, 10, 50)) === None)
 
     // touches on the right
-    assert(region(0, 10, 100).distance(region(0, 100, 200)) === Some(0))
+    assert(region(0, 10, 100).distance(region(0, 100, 200)) === Some(1))
 
     // overlaps
     assert(region(0, 10, 100).distance(region(0, 50, 150)) === Some(0))
 
     // touches on the left
-    assert(region(0, 10, 100).distance(region(0, 0, 10)) === Some(0))
+    assert(region(0, 10, 100).distance(region(0, 0, 10)) === Some(1))
   }
 
   test("distance(: ReferencePosition)") {
@@ -105,10 +106,10 @@ class ReferenceRegionSuite extends FunSuite {
     assert(region(0, 10, 100).distance(point(0, 10)) === Some(0))
 
     // right edge
-    assert(region(0, 10, 100).distance(point(0, 100)) === Some(0))
+    assert(region(0, 10, 100).distance(point(0, 100)) === Some(1))
 
     // right
-    assert(region(0, 10, 100).distance(point(0, 150)) === Some(50))
+    assert(region(0, 10, 100).distance(point(0, 150)) === Some(51))
 
     // left
     assert(region(0, 100, 200).distance(point(0, 50)) === Some(50))
@@ -116,6 +117,64 @@ class ReferenceRegionSuite extends FunSuite {
     // different sequences
     assert(region(0, 100, 200).distance(point(1, 50)) === None)
 
+  }
+
+  test("create region from unmapped read fails") {
+    val read = ADAMRecord.newBuilder()
+      .setReadMapped(false)
+      .build()
+
+    assert(ReferenceRegion(read).isEmpty)
+  }
+
+  test("create region from mapped read contains read start and end") {
+    val read = ADAMRecord.newBuilder()
+      .setReadMapped(true)
+      .setSequence("AAAAA")
+      .setStart(1L)
+      .setCigar("5M")
+      .setReferenceId(1)
+      .build()
+
+    assert(ReferenceRegion(read).isDefined)
+    assert(ReferenceRegion(read).get.contains(point(1, 1L)))
+    assert(ReferenceRegion(read).get.contains(point(1, 5L)))
+  }
+
+  test("validate that adjacent regions can be merged") {
+    val r1 = region(1, 0L, 6L)
+    val r2 = region(1, 6L, 10L)
+
+    assert(r1.distance(r2).get === 1)
+    assert(r1.isAdjacent(r2))
+    assert(r1.merge(r2) == region(1, 0L, 10L))
+  }
+
+  test("validate that non-adjacent regions cannot be merged") {
+    val r1 = region(1, 0L, 5L)
+    val r2 = region(1, 7L, 10L)
+
+    assert(!r1.isAdjacent(r2))
+
+    intercept[AssertionError] {
+      r1.merge(r2)
+    }
+  }
+
+  test("compute convex hull of two sets") {
+    val r1 = region(1, 0L, 5L)
+    val r2 = region(1, 7L, 10L)
+
+    assert(!r1.isAdjacent(r2))
+
+    val hull1 = r1.hull(r2)
+    val hull2 = r2.hull(r1)
+
+    assert(hull1 === hull2)
+    assert(hull1.overlaps(r1))
+    assert(hull1.overlaps(r2))
+    assert(hull1.start == 0L)
+    assert(hull1.end == 10L)
   }
 
   def region(refId: Int, start: Long, end: Long): ReferenceRegion =
