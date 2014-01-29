281127bfe99cfb2a49f3a426aa350d9d11ae69a2
Jey Kottalam <jey@cs.berkeley.edu>
2014-01-29 13:30:03 -0800

    Remove existing BQSR implementation

--- adam-core/src/main/scala/edu/berkeley/cs/amplab/adam/rdd/AdamRDDFunctions.scala
+++ adam-core/src/main/scala/edu/berkeley/cs/amplab/adam/rdd/AdamRDDFunctions.scala
@@ -102,8 +102,7 @@ class AdamRecordRDDFunctions(rdd: RDD[ADAMRecord]) extends Serializable with Log
   }
 
   def adamBQSR(dbSNP: SnpTable): RDD[ADAMRecord] = {
-    val broadcastDbSNP = rdd.context.broadcast(dbSNP)
-    RecalibrateBaseQualities(rdd, broadcastDbSNP)
+    throw new RuntimeException("not implemented")
   }
 
   def adamRealignIndels(): RDD[ADAMRecord] = {
