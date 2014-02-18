d83685b71cdb70334503195d00ae0946876f7020
Jeremy Elson <jelson@microsoft.com>
2014-02-18 15:03:54 -0800

    Fix copyright notice and add a little documentation

--- adam-core/src/main/java/edu/berkeley/cs/amplab/adam/io/InterleavedFastqInputFormat.java
+++ adam-core/src/main/java/edu/berkeley/cs/amplab/adam/io/InterleavedFastqInputFormat.java
@@ -1,24 +1,32 @@
-// Copyright (C) 2011-2012 CRS4.
-//
-// This file is part of Hadoop-BAM.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
+/*
+ * Copyright (c) 2013. Regents of the University of California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This class is a Hadoop reader for "interleaved fastq" -- that is,
+ * fastq with paired reads in the same file, interleaved, rather than
+ * in two separate files. This makes it much easier to Hadoopily slice
+ * up a single file and feed the slices into an aligner.
+ * The format is the same as fastq, but records are expected to alternate
+ * between /1 and /2.
+ *
+ * This reader is based on the FastqInputFormat that's part of Hadoop-BAM,
+ * found at http://sourceforge.net/p/hadoop-bam/code/ci/master/tree/src/fi/tkk/ics/hadoop/bam/FastqInputFormat.java
+ *
+ * --Jeremy Elson (jelson@microsoft.com), Feb 2014
+ */
 
 package edu.berkeley.cs.amplab.adam.io;
 
