f5caec07029fdcd42c03d691c041f5693dcb9289
Jeremy Elson <jelson@microsoft.com>
2014-02-18 17:50:16 -0800

    Change copyright notice and tabbing

--- adam-core/src/test/scala/edu/berkeley/cs/amplab/adam/io/InterleavedFastqInputFormatSuite.scala
+++ adam-core/src/test/scala/edu/berkeley/cs/amplab/adam/io/InterleavedFastqInputFormatSuite.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013. Regents of the University of California
+ * Copyright (c) 2014 Microsoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Author: Jeremy Elson (jelson@microsoft.com) Feb 2014
  */
 
 package edu.berkeley.cs.amplab.adam.rdd
@@ -27,34 +29,34 @@ import org.apache.hadoop.io.Text;
 
 
 class InterleavedFastqInputFormatSuite extends SparkFunSuite {
-	(1 to 4) foreach { testNumber =>
-		val inputName = "interleaved_fastq_sample%d.fq".format(testNumber);
-		val expectedOutputName = inputName + ".output";
-		val expectedOutputPath = ClassLoader.getSystemClassLoader.getResource(expectedOutputName).getFile;
-		val expectedOutputData = scala.io.Source.fromFile(expectedOutputPath).mkString;
+  (1 to 4) foreach { testNumber =>
+    val inputName = "interleaved_fastq_sample%d.fq".format(testNumber);
+    val expectedOutputName = inputName + ".output";
+    val expectedOutputPath = ClassLoader.getSystemClassLoader.getResource(expectedOutputName).getFile;
+    val expectedOutputData = scala.io.Source.fromFile(expectedOutputPath).mkString;
 
-		sparkTest("interleaved FASTQ hadoop reader: %s->%s".format(inputName, expectedOutputName)) {
-			def ifq_reader: RDD[(Void, Text)] = {
-				val path = ClassLoader.getSystemClassLoader.getResource(inputName).getFile
-				sc.newAPIHadoopFile(path,
-						    classOf[InterleavedFastqInputFormat],
-						    classOf[Void],
-						    classOf[Text])
-			}
+    sparkTest("interleaved FASTQ hadoop reader: %s->%s".format(inputName, expectedOutputName)) {
+      def ifq_reader: RDD[(Void, Text)] = {
+        val path = ClassLoader.getSystemClassLoader.getResource(inputName).getFile
+        sc.newAPIHadoopFile(path,
+                classOf[InterleavedFastqInputFormat],
+                classOf[Void],
+                classOf[Text])
+      }
 
-			val ifq_reads = ifq_reader.collect()
+      val ifq_reads = ifq_reader.collect()
 
-			val testOutput = new StringBuilder();
+      val testOutput = new StringBuilder();
 
-			ifq_reads.foreach(pair => {
-					testOutput.append(">>>interleaved fastq record start>>>\n");
-					testOutput.append(pair._2);
-					testOutput.append("<<<interleaved fastq record end<<<\n");
-				});
+      ifq_reads.foreach(pair => {
+          testOutput.append(">>>interleaved fastq record start>>>\n");
+          testOutput.append(pair._2);
+          testOutput.append("<<<interleaved fastq record end<<<\n");
+        });
 
-			assert(testOutput.toString() == expectedOutputData);
-			// System.out.println("%s result:\n".format(inputName) + testOutput.toString());
-		}
-	}
+      assert(testOutput.toString() == expectedOutputData);
+      // System.out.println("%s result:\n".format(inputName) + testOutput.toString());
+    }
+  }
 }
 
