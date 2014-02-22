854d43210a4ddbae83148cc7765e0c7545c4bab8
Jey Kottalam <jey@cs.berkeley.edu>
2014-02-22 00:01:11 -0800

    Cleanup Observation.empiricalQuality

--- adam-core/src/main/scala/edu/berkeley/cs/amplab/adam/rdd/recalibration/ObservationTable.scala
+++ adam-core/src/main/scala/edu/berkeley/cs/amplab/adam/rdd/recalibration/ObservationTable.scala
@@ -30,24 +30,21 @@ class Observation(val total: Long, val mismatches: Long) extends Serializable {
   def +(that: Observation) =
     new Observation(this.total + that.total, this.mismatches + that.mismatches)
 
-  def empiricalQuality: QualityScore = bayesianQualityEstimate
+  /**
+   * Empirically estimated probability of a mismatch.
+   */
+  def empiricalQuality: QualityScore = empiricalQuality(1, 1)
 
-  // Estimates the probability of a mismatch under a Bayesian model with Binomial likelihood
-  // and Beta(a, b) prior. When a = b = 1, this is also known as "Laplace's rule of succession".
-  // TODO: Beta(1, 1) is the safest choice, but maybe Beta(1/2, 1/2) is more accurate?
-  def bayesianQualityEstimate: QualityScore = bayesianQualityEstimate(1, 1)
-  def bayesianQualityEstimate(a: Double, b: Double): QualityScore =
+  /**
+   * Estimates the probability of a mismatch under a Bayesian model with
+   * Binomial likelihood and Beta(a, b) prior. When a = b = 1, this is also
+   * known as "Laplace's rule of succession".
+   *
+   * TODO: Beta(1, 1) is the safest choice, but maybe Beta(1/2, 1/2) is more
+   * accurate?
+   */
+  def empiricalQuality(a: Double, b: Double): QualityScore =
     QualityScore.fromErrorProbability((a + mismatches) / (a + b + total))
-
-  // GATK 1.6 uses the following, which they describe as "Yates's correction". However,
-  // it doesn't match the Wikipedia entry which describes a different formula used
-  // for correction of chi-squared independence tests on contingency tables.
-  // TODO: Figure out this discrepancy.
-  def gatkQualityEstimate: QualityScore = gatkQualityEstimate(1)
-  def gatkQualityEstimate(smoothing: Int): QualityScore =
-    Seq(QualityScore(50), gatkQualityEstimateUnclipped(smoothing)).min
-  def gatkQualityEstimateUnclipped(smoothing: Int): QualityScore =
-    QualityScore.fromErrorProbability((smoothing + mismatches) / (smoothing + total))
 
   // Format as string compatible with GATK's CSV output
   def toCSV: String = "%s,%s,%s".format(total, mismatches, empiricalQuality.phred)
