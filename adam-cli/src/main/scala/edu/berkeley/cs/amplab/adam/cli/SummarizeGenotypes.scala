ce7f5c358e8fa5143dd23d5a9e64a12538b7cda2
Tim O'Donnell <timodonnell@gmail.com>
2014-03-21 12:36:35 -0400

    set copyright on new files to Mount Sinai

--- adam-cli/src/main/scala/edu/berkeley/cs/amplab/adam/cli/SummarizeGenotypes.scala
+++ adam-cli/src/main/scala/edu/berkeley/cs/amplab/adam/cli/SummarizeGenotypes.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013. Regents of the University of California
+ * Copyright (c) 2013. Mount Sinai School of Medicine
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
