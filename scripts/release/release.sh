d0821c74ac18d3930d4028d050fd5b6ee0bd9fa9
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2015-01-12 08:55:39 -0800

    [ADAM-536] Add support for scala 2.11

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 mvn -P distribution -Dresume=false release:clean release:prepare release:perform
+
+mvn -P distribution,scala-2.11 -Dresume=false release:clean release:prepare release:perform
