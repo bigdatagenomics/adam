1011fab9dafdf597e993ab48509e7299f1ce1698
Michael Heuer <heuermh@acm.org>
2015-07-08 17:59:59 -0500

    [ADAM-723] removing no longer revelant files from scripts directory

--- scripts/adam-console.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_ROOT="$DIR/.."
-VERSION=$(grep "<version>" "$PROJECT_ROOT/pom.xml"  | head -2 | tail -1 | sed 's/ *<version>//g' | sed 's/<\/version>//g')
-ADAM_JAR=$PROJECT_ROOT/adam-cli/target/adam-$VERSION.jar
-
-SPARK_CLASSPATH=$ADAM_JAR spark-shell -i:$DIR/scala-console.scala
