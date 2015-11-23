704d2ccd6eaad2e856fdec2d5ed2395edef98c04
Michael L Heuer <heuermh@acm.org>
2015-11-23 11:25:33 -0600

    add exception for maven-javadoc-plugin, fixes #863

--- scripts/move_to_scala_2.11.sh
+++ scripts/move_to_scala_2.11.sh
@@ -3,5 +3,8 @@
 set +x
 
 find . -name "pom.xml" -exec sed -e "s/2.10.4/2.11.4/g" -e "s/2.10/2.11/g" -i .2.11.bak '{}' \;
+# keep parquet-scala at parquet-scala_2.10
 find . -name "pom.xml" -exec sed -e "s/parquet-scala_2.11/parquet-scala_2.10/g" -i .2.11.2.bak '{}' \;
+# keep maven-javadoc-plugin at version 2.10.3
+find . -name "pom.xml" -exec sed -e "s/2.11.3/2.10.3/g" -i .2.11.3.bak '{}' \;
 find . -name "*.2.11.*bak" -exec rm {} \;
