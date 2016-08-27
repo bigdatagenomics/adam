cd488dfd1ace53bc3aec0e694def59b8ab2c1344
Ryan Williams <ryan.blake.williams@gmail.com>
2016-08-27 18:15:04 +0000

    Cleaning up POM:
    * bump scala versions
    * bump resources-plugin version
    * rename "surefire" (scalatest) reports directory
    * make scala.version.prefix a pom property

--- scripts/move_to_scala_2.10.sh
+++ scripts/move_to_scala_2.10.sh
@@ -2,5 +2,5 @@
 
 set +x
 
-find . -name "pom.xml" -exec sed -e "s/2.11.4/2.10.4/g" -e "s/2.11/2.10/g" -i .2.10.bak '{}' \;
+find . -name "pom.xml" -exec sed -e "s/2.11.8/2.10.6/g" -e "s/2.11/2.10/g" -i .2.10.bak '{}' \;
 find . -name "*.2.10.bak" -exec rm {} \;
