1b92e6edc385814ba77e919cd00a3337ff5b9137
Zhuyi Xue <alfred532008@gmail.com>
2016-09-27 20:21:23 -0700

    updated a testcase for FastqRecordConverter.convertPair

--- adam-core/src/test/scala/org/bdgenomics/adam/converters/FastqConverterSuite.scala
+++ adam-core/src/test/scala/org/bdgenomics/adam/converters/FastqConverterSuite.scala
@@ -9,11 +9,11 @@ import org.scalatest.FunSuite
 class FastqConverterSuite extends FunSuite {
   val converter = new FastqRecordConverter
 
-  test("testing convert fastq record pair to AlignmentRecord iterator") {
+  test("testing FastqRecordConverter.convertPair with valid input") {
     val input = (null, new Text("@read/1\nATCGA\n+\nabcde\n@read/2\nTCGAT\n+\n12345"))
     val alignRecIterator = converter.convertPair(input)
 
-    assert(alignRecIterator.map(a => a.getReadName) === List("read/1", "read/2"))
+    assert(alignRecIterator.map(a => a.getReadName) === List("read", "read"))
     assert(alignRecIterator.map(a => a.getSequence) === List("ATCGA", "TCGAT"))
     assert(alignRecIterator.map(a => a.getQual) === List("abcde", "12345"))
     assert(alignRecIterator.map(a => a.getReadPaired) === List(true, true))
