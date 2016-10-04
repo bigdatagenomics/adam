a0730f36655d8fbd5359d84b5564c83c90ff7cf5
Ryan Williams <ryan.blake.williams@gmail.com>
2016-10-04 19:23:19 +0000

    fix sed "-i" args

--- scripts/move_to_spark_2.sh
+++ scripts/move_to_spark_2.sh
@@ -11,4 +11,4 @@ find . -name "pom.xml" -exec sed \
     -e "/utils-/ $substitution_cmd" \
     -e "/adam-/ $substitution_cmd" \
     -e "/spark\.version/ s/1\.6\.1/2.0.0/g" \
-    -i .spark2.bak '{}' \;
+    -i.spark2.bak '{}' \;
