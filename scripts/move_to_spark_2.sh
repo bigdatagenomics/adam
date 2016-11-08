1b72f0ba98d56fbd723ea043ab830d201ed6d6b5
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-11-08 15:05:32 -0800

    [ADAM-1221] Bump Spark/Hadoop versions.
    Resolves #1221.

--- scripts/move_to_spark_2.sh
+++ scripts/move_to_spark_2.sh
@@ -2,7 +2,7 @@
 
 set +x
 
-svp="\${scala\.version\.prefix}"
+svp="\${scala.version.prefix}"
 substitution_cmd="s/_$svp/-spark2_$svp/g"
 
 find . -name "pom.xml" -exec sed \
@@ -10,5 +10,5 @@ find . -name "pom.xml" -exec sed \
     -e "/adam-/ s/_2\.10/-spark2_2.10/g" \
     -e "/utils-/ $substitution_cmd" \
     -e "/adam-/ $substitution_cmd" \
-    -e "/spark\.version/ s/1\.6\.1/2.0.0/g" \
+    -e "/spark.version/ s/1.6.3/2.0.0/g" \
     -i.spark2.bak '{}' \;
