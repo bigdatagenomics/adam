835259213bd1bf077d0804bb480df7b40eb7454d
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-11-10 06:26:57 -0800

    Revert "[ADAM-1221] Bump Spark/Hadoop versions."
    This reverts commit 19d0b6fe08e43f5be288fc2dcad41efc4f9f7704.

--- scripts/move_to_spark_2.sh
+++ scripts/move_to_spark_2.sh
@@ -6,7 +6,9 @@ svp="\${scala\.version\.prefix}"
 substitution_cmd="s/_$svp/-spark2_$svp/g"
 
 find . -name "pom.xml" -exec sed \
+    -e "/utils-/ s/_2\.10/-spark2_2.10/g" \
+    -e "/adam-/ s/_2\.10/-spark2_2.10/g" \
     -e "/utils-/ $substitution_cmd" \
     -e "/adam-/ $substitution_cmd" \
-    -e "/spark.version/ s/1.6.3/2.0.0/g" \
+    -e "/spark\.version/ s/1\.6\.1/2.0.0/g" \
     -i.spark2.bak '{}' \;
