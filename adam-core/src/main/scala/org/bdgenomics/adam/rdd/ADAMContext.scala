5dee5d5d8b070ad6843f83efd2ab59215a6d6246
Ryan Williams <ryan.blake.williams@gmail.com>
2016-11-17 17:38:05 +0000

    misc style nits

--- adam-core/src/main/scala/org/bdgenomics/adam/rdd/ADAMContext.scala
+++ adam-core/src/main/scala/org/bdgenomics/adam/rdd/ADAMContext.scala
@@ -91,7 +91,7 @@ object ADAMContext {
   implicit def sameTypeConversionFn[T, U <: GenomicRDD[T, U]](gRdd: U,
                                                               rdd: RDD[T]): U = {
     // hijack the transform function to discard the old RDD
-    gRdd.transform(oldRdd => rdd)
+    gRdd.transform(_ => rdd)
   }
 
   implicit def readsToVCConversionFn(arRdd: AlignmentRecordRDD,
@@ -136,8 +136,6 @@ private class FileFilter(private val name: String) extends PathFilter {
     path.getName == name
   }
 }
-
-import org.bdgenomics.adam.rdd.ADAMContext._
 
 /**
  * The ADAMContext provides functions on top of a SparkContext for loading genomic data.
@@ -187,12 +185,13 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
   private def loadSingleVcfMetadata(filePath: String): (SequenceDictionary, Seq[Sample]) = {
     def headerToMetadata(vcfHeader: VCFHeader): (SequenceDictionary, Seq[Sample]) = {
       val sd = SequenceDictionary.fromVCFHeader(vcfHeader)
-      val samples = asScalaBuffer(vcfHeader.getGenotypeSamples)
-        .map(s => {
-          Sample.newBuilder()
-            .setSampleId(s)
-            .build()
-        }).toSeq
+      val samples =
+        asScalaBuffer(vcfHeader.getGenotypeSamples)
+          .map(s =>
+            Sample.newBuilder()
+              .setSampleId(s)
+              .build()
+          )
       (sd, samples)
     }
 
@@ -445,12 +444,11 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
             val rg = loadBamReadGroups(samHeader)
             Some((sd, rg))
           } catch {
-            case e: Throwable => {
+            case e: Throwable =>
               log.error(
                 s"Loading failed for $fp:n${e.getMessage}\n\t${e.getStackTrace.take(25).map(_.toString).mkString("\n\t")}"
               )
               None
-            }
           }
         }).reduce((kv1, kv2) => {
           (kv1._1 ++ kv2._1, kv1._2 ++ kv2._2)
@@ -465,7 +463,7 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
     // contains bams, hadoop-bam is a-ok! i believe that it is better (perf) to
     // just load from a single newAPIHadoopFile call instead of a union across
     // files, so we do that whenever possible
-    val records = if (filteredFiles.size != bamFiles.size) {
+    val records = if (filteredFiles.length != bamFiles.length) {
       sc.union(filteredFiles.map(p => {
         sc.newAPIHadoopFile(p.toString, classOf[AnySAMInputFormat], classOf[LongWritable],
           classOf[SAMRecordWritable], ContextUtil.getConfiguration(job))
@@ -474,7 +472,10 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
       sc.newAPIHadoopFile(filePath, classOf[AnySAMInputFormat], classOf[LongWritable],
         classOf[SAMRecordWritable], ContextUtil.getConfiguration(job))
     }
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
+
     val samRecordConverter = new SAMRecordConverter
 
     AlignedReadRDD(records.map(p => samRecordConverter.convert(p._2.get)),
@@ -532,7 +533,10 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
       sc.newAPIHadoopFile(p.toString, classOf[BAMInputFormat], classOf[LongWritable],
         classOf[SAMRecordWritable], conf)
     }))
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
+
     val samRecordConverter = new SAMRecordConverter
     AlignedReadRDD(records.map(p => samRecordConverter.convert(p._2.get)),
       seqDict,
@@ -612,7 +616,6 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
 
     // reverse list and return as seq
     list.reverse
-      .toSeq
   }
 
   /**
@@ -669,7 +672,9 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
       classOf[Text],
       ContextUtil.getConfiguration(job)
     )
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
 
     // convert records
     val fastqRecordConverter = new FastqRecordConverter
@@ -780,7 +785,9 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
       classOf[Text],
       ContextUtil.getConfiguration(job)
     )
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
 
     // convert records
     val fastqRecordConverter = new FastqRecordConverter
@@ -839,7 +846,8 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
     val records = readVcfRecords(filePath, None)
 
     // attach instrumentation
-    if (Metrics.isRecording) records.instrument() else records
+    if (Metrics.isRecording)
+      records.instrument()
 
     // load vcf metadata
     val (sd, samples) = loadVcfMetadata(filePath)
@@ -876,7 +884,8 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
     val records = readVcfRecords(filePath, Some(viewRegions))
 
     // attach instrumentation
-    if (Metrics.isRecording) records.instrument() else records
+    if (Metrics.isRecording)
+      records.instrument()
 
     // load vcf metadata
     val (sd, samples) = loadVcfMetadata(filePath)
@@ -946,7 +955,9 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
       classOf[LongWritable],
       classOf[Text]
     )
-    if (Metrics.isRecording) fastaData.instrument() else fastaData
+
+    if (Metrics.isRecording)
+      fastaData.instrument()
 
     val remapData = fastaData.map(kv => (kv._1.get, kv._2.toString))
 
@@ -978,7 +989,9 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
       classOf[Text],
       ContextUtil.getConfiguration(job)
     )
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
 
     // convert records
     val fastqRecordConverter = new FastqRecordConverter
@@ -1024,7 +1037,10 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
                stringency: ValidationStringency = ValidationStringency.LENIENT): FeatureRDD = {
     val records = sc.textFile(filePath, minPartitions.getOrElse(sc.defaultParallelism))
       .flatMap(new GFF3Parser().parse(_, stringency))
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
+
     FeatureRDD(records)
   }
 
@@ -1044,7 +1060,10 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
               stringency: ValidationStringency = ValidationStringency.LENIENT): FeatureRDD = {
     val records = sc.textFile(filePath, minPartitions.getOrElse(sc.defaultParallelism))
       .flatMap(new GTFParser().parse(_, stringency))
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
+
     FeatureRDD(records)
   }
 
@@ -1064,7 +1083,10 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
               stringency: ValidationStringency = ValidationStringency.LENIENT): FeatureRDD = {
     val records = sc.textFile(filePath, minPartitions.getOrElse(sc.defaultParallelism))
       .flatMap(new BEDParser().parse(_, stringency))
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
+
     FeatureRDD(records)
   }
 
@@ -1084,7 +1106,10 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
                      stringency: ValidationStringency = ValidationStringency.LENIENT): FeatureRDD = {
     val records = sc.textFile(filePath, minPartitions.getOrElse(sc.defaultParallelism))
       .flatMap(new NarrowPeakParser().parse(_, stringency))
-    if (Metrics.isRecording) records.instrument() else records
+
+    if (Metrics.isRecording)
+      records.instrument()
+
     FeatureRDD(records)
   }
 
@@ -1105,11 +1130,16 @@ class ADAMContext private (@transient val sc: SparkContext) extends Serializable
 
     val parsedLines = sc.textFile(filePath, minPartitions.getOrElse(sc.defaultParallelism))
       .map(new IntervalListParser().parseWithHeader(_, stringency))
-    val (seqDict, records) = (SequenceDictionary(parsedLines.flatMap(_._1).collect(): _*),
-      parsedLines.flatMap(_._2))
+
+    val seqDict = SequenceDictionary(parsedLines.flatMap(_._1).collect(): _*)
+
+    val records = parsedLines.flatMap(_._2)
+
     val seqDictMap = seqDict.records.map(sr => sr.name -> sr).toMap
 
-    if (Metrics.isRecording) records.instrument() else records
+    if (Metrics.isRecording)
+      records.instrument()
+
     FeatureRDD(records, seqDict)
   }
 
