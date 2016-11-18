ab1cb72c19a87c31136fce635544fbd9e347f881
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-11-17 22:03:08 -0800

    Line wrap SupportedHeaderLines to break long lines.

--- adam-core/src/main/scala/org/bdgenomics/adam/converters/SupportedHeaderLines.scala
+++ adam-core/src/main/scala/org/bdgenomics/adam/converters/SupportedHeaderLines.scala
@@ -28,20 +28,62 @@ import htsjdk.variant.vcf.{
  */
 private[adam] object SupportedHeaderLines {
 
-  lazy val ancestralAllele = new VCFInfoHeaderLine("AA", 1, VCFHeaderLineType.String, "Ancestral allele");
-  lazy val alleleCount = new VCFInfoHeaderLine("AC", VCFHeaderLineCount.A, VCFHeaderLineType.Integer, "Allele count");
-  lazy val readDepth = new VCFInfoHeaderLine("AD", VCFHeaderLineCount.R, VCFHeaderLineType.Integer, "Total read depths for each allele");
-  lazy val forwardReadDepth = new VCFInfoHeaderLine("ADF", VCFHeaderLineCount.R, VCFHeaderLineType.Integer, "Read depths for each allele on the forward strand");
-  lazy val reverseReadDepth = new VCFInfoHeaderLine("ADR", VCFHeaderLineCount.R, VCFHeaderLineType.Integer, "Read depths for each allele on the reverse strand");
-  lazy val alleleFrequency = new VCFInfoHeaderLine("AF", VCFHeaderLineCount.A, VCFHeaderLineType.Float, "Allele frequency for each allele");
-  lazy val cigar = new VCFInfoHeaderLine("CIGAR", VCFHeaderLineCount.A, VCFHeaderLineType.String, "Cigar string describing how to align alternate alleles to the reference allele");
-  lazy val dbSnp = new VCFInfoHeaderLine("DB", 0, VCFHeaderLineType.Flag, "Membership in dbSNP");
-  lazy val hapMap2 = new VCFInfoHeaderLine("H2", 0, VCFHeaderLineType.Flag, "Membership in HapMap2");
-  lazy val hapMap3 = new VCFInfoHeaderLine("H3", 0, VCFHeaderLineType.Flag, "Membership in HapMap3");
-  lazy val validated = new VCFInfoHeaderLine("VALIDATED", 0, VCFHeaderLineType.Flag, "Validated by follow-up experiment");
-  lazy val thousandGenomes = new VCFInfoHeaderLine("1000G", 0, VCFHeaderLineType.Flag, "Membership in 1000 Genomes");
-  lazy val somatic = new VCFInfoHeaderLine("SOMATIC", 0, VCFHeaderLineType.Flag, "Somatic event");
-  lazy val transcriptEffects = new VCFInfoHeaderLine("ANN", VCFHeaderLineCount.UNBOUNDED, VCFHeaderLineType.String, "Functional annotations: 'Allele | Annotation | Annotation_Impact | Gene_Name | Gene_ID | Feature_Type | Feature_ID | Transcript_BioType | Rank | HGVS.c | HGVS.p | cDNA.pos / cDNA.length | CDS.pos / CDS.length | AA.pos / AA.length | Distance | ERRORS / WARNINGS / INFO'");
+  lazy val ancestralAllele = new VCFInfoHeaderLine("AA",
+    1,
+    VCFHeaderLineType.String,
+    "Ancestral allele");
+  lazy val alleleCount = new VCFInfoHeaderLine("AC",
+    VCFHeaderLineCount.A,
+    VCFHeaderLineType.Integer,
+    "Allele count");
+  lazy val readDepth = new VCFInfoHeaderLine("AD",
+    VCFHeaderLineCount.R,
+    VCFHeaderLineType.Integer,
+    "Total read depths for each allele");
+  lazy val forwardReadDepth = new VCFInfoHeaderLine("ADF",
+    VCFHeaderLineCount.R,
+    VCFHeaderLineType.Integer,
+    "Read depths for each allele on the forward strand");
+  lazy val reverseReadDepth = new VCFInfoHeaderLine("ADR",
+    VCFHeaderLineCount.R,
+    VCFHeaderLineType.Integer,
+    "Read depths for each allele on the reverse strand");
+  lazy val alleleFrequency = new VCFInfoHeaderLine("AF",
+    VCFHeaderLineCount.A,
+    VCFHeaderLineType.Float,
+    "Allele frequency for each allele");
+  lazy val cigar = new VCFInfoHeaderLine("CIGAR",
+    VCFHeaderLineCount.A,
+    VCFHeaderLineType.String,
+    "Cigar string describing how to align alternate alleles to the reference allele");
+  lazy val dbSnp = new VCFInfoHeaderLine("DB",
+    0,
+    VCFHeaderLineType.Flag,
+    "Membership in dbSNP");
+  lazy val hapMap2 = new VCFInfoHeaderLine("H2",
+    0,
+    VCFHeaderLineType.Flag,
+    "Membership in HapMap2");
+  lazy val hapMap3 = new VCFInfoHeaderLine("H3",
+    0,
+    VCFHeaderLineType.Flag,
+    "Membership in HapMap3");
+  lazy val validated = new VCFInfoHeaderLine("VALIDATED",
+    0,
+    VCFHeaderLineType.Flag,
+    "Validated by follow-up experiment");
+  lazy val thousandGenomes = new VCFInfoHeaderLine("1000G",
+    0,
+    VCFHeaderLineType.Flag,
+    "Membership in 1000 Genomes");
+  lazy val somatic = new VCFInfoHeaderLine("SOMATIC",
+    0,
+    VCFHeaderLineType.Flag,
+    "Somatic event");
+  lazy val transcriptEffects = new VCFInfoHeaderLine("ANN",
+    VCFHeaderLineCount.UNBOUNDED,
+    VCFHeaderLineType.String,
+    "Functional annotations: 'Allele | Annotation | Annotation_Impact | Gene_Name | Gene_ID | Feature_Type | Feature_ID | Transcript_BioType | Rank | HGVS.c | HGVS.p | cDNA.pos / cDNA.length | CDS.pos / CDS.length | AA.pos / AA.length | Distance | ERRORS / WARNINGS / INFO'");
 
   /**
    * All info keys in VCF format.
