bf136e5ff75f75825a82cf01a707909841589423
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-11-29 08:46:28 -0800

    Add -U flag to jenkins-test.

--- scripts/move_to_scala_2.11.sh
+++ scripts/move_to_scala_2.11.sh
@@ -2,7 +2,7 @@
 
 set +x
 
-find . -name "pom.xml" -exec sed -e "s/2.10.6/2.11.8/g" -e "s/2.10/2.11/g" -i.2.11.bak '{}' \;
+find . -name "pom.xml" -exec sed -e "s/2.10.6/2.11.8/g" -e "/bdg-utils.version/!s/2.10/2.11/g" -i.2.11.bak '{}' \;
 # keep parquet-scala at parquet-scala_2.10
 find . -name "pom.xml" -exec sed -e "s/parquet-scala_2.11/parquet-scala_2.10/g" -i.2.11.2.bak '{}' \;
 # keep maven-javadoc-plugin at version 2.10.3
