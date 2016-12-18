d774073b965ed0c9842aaddd63c460a9ba8fa8d5
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-12-17 22:55:48 -0800

    [ADAM-1317] Add interlocks for move_to_xyz scripts.
    Resolves #1317.

--- scripts/move_to_spark_1.sh
+++ scripts/move_to_spark_1.sh
@@ -2,6 +2,15 @@
 
 set +x
 
+grep -q "spark2" pom.xml
+if [[ $? == 1 ]];
+then
+    echo "POM is already set up for Spark 1 (Spark 1/2 artifacts are missing -spark2 suffix in artifact names)."
+    echo "Cowardly refusing to move to Spark 1 a second time..."
+
+    exit 1
+fi
+
 find . -name "pom.xml" -exec sed \
     -e "s/-spark2//g" \
     -e "/spark.version/ s/2.0.0/1.6.3/g" \
