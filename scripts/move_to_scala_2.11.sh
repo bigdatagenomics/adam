1c00da56e17deb38229329d8678a041170ed18d3
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-12-17 22:55:48 -0800

    [ADAM-1317] Add interlocks for move_to_xyz scripts.
    Resolves #1317.

--- scripts/move_to_scala_2.11.sh
+++ scripts/move_to_scala_2.11.sh
@@ -2,9 +2,20 @@
 
 set +x
 
-find . -name "pom.xml" -exec sed -e "s/2.10.6/2.11.8/g" -e "/bdg-utils.version/!s/2.10/2.11/g" -i.2.11.bak '{}' \;
+grep "<scala\.version>" pom.xml | grep -q 2.11
+if [[ $? == 0 ]];
+then
+    echo "Scala version is already set to 2.11 (Scala artifacts have _2.11 version suffix in artifact name)."
+    echo "Cowardly refusing to move to Scala 2.11 a second time..."
+
+    exit 1
+fi
+
+find . -name "pom.xml" -exec sed -e "s/2.10.6/2.11.8/g" \
+    -e "/bdg-utils.version/! s/2.10/2.11/g" \
+    -i.2.11.bak '{}' \;
 # keep parquet-scala at parquet-scala_2.10
 find . -name "pom.xml" -exec sed -e "s/parquet-scala_2.11/parquet-scala_2.10/g" -i.2.11.2.bak '{}' \;
 # keep maven-javadoc-plugin at version 2.10.3
 find . -name "pom.xml" -exec sed -e "s/2.11.3/2.10.3/g" -i.2.11.3.bak '{}' \;
-find . -name "*.2.11.*bak" -exec rm {} \;
+find . -name "*.2.11.*bak" -exec rm -f {} \;
