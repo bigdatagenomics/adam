640c44bae1841217f981899c3a2b793c7ae06dcd
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2017-02-20 23:16:51 -0800

    [ADAM-882] Add R API.
    Resolves #882. Adds an R API that binds around the ADAM GenomicRDD APIs.
    Supports Spark 2.x and onwards, as accessible SparkR functionality for binding
    to Java libraries was added in Spark 2.0.0.

---
+++ adam-r/bdg.adam/tests/testthat/test_alignmentRecordRdd.R
@@ -0,0 +1,69 @@
+#
+# Licensed to Big Data Genomics (BDG) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The BDG licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+library(bdg.adam)
+
+context("manipulating alignmentrecords")
+
+sc <- sparkR.session()
+ac <- ADAMContext(sc)
+
+test_that("save sorted sam", {
+
+    originalReads <- resourceFile("sorted.sam")
+    reads <- loadAlignments(ac, originalReads)
+    tmpPath <- tempfile(fileext = ".sam")
+    sortedReads <- sortReadsByReferencePosition(reads)
+    saveAsSam(reads, tmpPath, isSorted = TRUE, asSingleFile = TRUE)
+
+    expect_files_match(tmpPath, originalReads)
+})
+
+test_that("save unordered sam", {
+
+    originalReads <- resourceFile("unordered.sam")
+    reads <- loadAlignments(ac, originalReads)
+    tmpPath <- tempfile(fileext = ".sam")
+    sortedReads <- sortReadsByReferencePosition(reads)
+    saveAsSam(reads, tmpPath, asSingleFile = TRUE)
+
+    expect_files_match(tmpPath, originalReads)
+})
+
+test_that("save as bam", {
+
+    originalReads <- resourceFile("sorted.sam")
+    reads <- loadAlignments(ac, originalReads)
+    tmpPath <- tempfile(fileext = ".bam")
+    sortedReads <- sortReadsByReferencePosition(reads)
+    saveAsSam(reads, tmpPath, isSorted = TRUE, asSingleFile = TRUE)
+
+    bam <- loadAlignments(ac, tmpPath)
+    readsDf <- toDF(reads)
+    bamDf <- toDF(bam)
+    
+    expect_equal(count(readsDf), count(bamDf))
+})
+
+test_that("count k-mers", {
+
+    originalReads <- resourceFile("small.sam")
+    reads <- loadAlignments(ac, originalReads)
+    kmers <- countKmers(reads, 6)
+
+    expect_equal(count(kmers), 1040)
+})
---
+++ adam-r/bdg.adam/tests/testthat/test_genotypeRdd.R
@@ -0,0 +1,54 @@
+#
+# Licensed to Big Data Genomics (BDG) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The BDG licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+library(bdg.adam)
+
+context("manipulating genotypes")
+
+sc <- sparkR.session()
+ac <- ADAMContext(sc)
+
+test_that("round trip vcf", {
+    testFile <- resourceFile("small.vcf")
+    genotypes <- loadGenotypes(ac, testFile)
+    tmpPath <- tempfile(fileext = ".vcf")
+    save(genotypes, tmpPath)
+
+    expect_equal(count(toDF(genotypes)), count(toDF(loadGenotypes(ac, tmpPath))))
+})
+
+test_that("save sorted vcf", {
+
+    testFile <- resourceFile("random.vcf")
+    genotypes <- loadGenotypes(ac, testFile)
+    tmpPath <- tempfile(fileext = ".vcf")
+    saveAsVcf(genotypes, tmpPath, asSingleFile = TRUE, sortOnSave = TRUE)
+
+    truthFile <- resourceFile("sorted.vcf")
+    expect_files_match(tmpPath, truthFile)
+})
+
+test_that("save lex sorted vcf", {
+
+    testFile <- resourceFile("random.vcf")
+    genotypes <- loadGenotypes(ac, testFile)
+    tmpPath <- tempfile(fileext = ".vcf")
+    saveAsVcf(genotypes, tmpPath, asSingleFile = TRUE, sortOnSave = "lexicographically")
+
+    truthFile <- resourceFile("sorted.lex.vcf")
+    expect_files_match(tmpPath, truthFile)
+})
