3a6b0bbcf6da1b058645bfed0303097c81924034
xubo245 <xubo245@mail.ustc.edu.cn>
2017-03-18 16:39:21 +0800

    improve  pull request quality: change Field name

--- adam-core/src/main/scala/org/bdgenomics/adam/util/Constants.scala
+++ adam-core/src/main/scala/org/bdgenomics/adam/util/Constants.scala
@@ -22,5 +22,5 @@ package org.bdgenomics.adam.util
  */
 object Constants {
   // design for org.bdgenomics.adam.rdd.ADAMContext.loadReadsFromSamString
-  val num_required_fields = 11
+  val num_Required_Fields = 11
 }
