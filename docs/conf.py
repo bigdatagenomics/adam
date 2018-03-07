8e80e44e469844740d18f0e7fed1bf0c213eef36
Frank Austin Nothaft <frank.nothaft@databricks.com>
2018-03-06 23:42:12 -0800

    [ADAM-1917] Set minimal Sphinx version for Readthedocs build.
    Resolves #1917.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ bdgVersion = '0.23.0-SNAPSHOT'
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.5.6'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
