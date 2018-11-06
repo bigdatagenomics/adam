5ad494156472d82f8628d465b4038dd3778b745d
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-11-06 17:13:42 -0500

    ADAM-2079 Adds 'N' to the regex for both the input format and record converter. Also changes the test data to include an 'N'

--- adam-core/src/test/scala/org/bdgenomics/adam/rdd/fragment/FragmentRDDSuite.scala
+++ adam-core/src/test/scala/org/bdgenomics/adam/rdd/fragment/FragmentRDDSuite.scala
@@ -588,4 +588,26 @@ class FragmentRDDSuite extends ADAMFunSuite {
       assert(fragment.getAlignments.size() == 2)
     })
   }
+
+  sparkTest("interleaved paired read names with index sequences in read names can group into fragments") {
+    val path = testFile("read_names_with_index_sequences_interleaved.fq")
+    val fragments = sc.loadInterleavedFastq(path).toFragments()
+
+    assert(fragments.rdd.count() == 4)
+
+    fragments.rdd.collect().foreach(fragment => {
+      assert(fragment.getAlignments.size() == 2)
+    })
+  }
+
+  sparkTest("interleaved paired read names with index sequences in read names as fragments") {
+    val path = testFile("read_names_with_index_sequences_interleaved.fq")
+    val fragments = sc.loadInterleavedFastqAsFragments(path)
+
+    assert(fragments.rdd.count() == 4)
+
+    fragments.rdd.collect().foreach(fragment => {
+      assert(fragment.getAlignments.size() == 2)
+    })
+  }
 }
