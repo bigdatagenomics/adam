39fbad5632750a31e0073ca89a75b53e922991aa
Michael L Heuer <heuermh@acm.org>
2019-05-18 20:08:00 -0500

    moar

--- adam-python/bdgenomics/adam/test/__init__.py
+++ adam-python/bdgenomics/adam/test/__init__.py
@@ -60,7 +60,7 @@ class SparkTestCase(unittest.TestCase):
         self.ss = SparkSession.builder \
                               .master('local[4]') \
                               .appName(class_name) \
-                              .config('spark.executor.memory', '4g') \
+                              .config('spark.executor.memory', '8g') \
                               .getOrCreate()
         self.sc = self.ss.sparkContext
 
