edef874245565c97a998e82728804b11d2b82eab
Michael L Heuer <heuermh@acm.org>
2020-01-06 11:28:29 -0600

    Commit template substitution may not be available if building from tarball.

--- adam-cli/src/test/scala/org/bdgenomics/adam/cli/AboutSuite.scala
+++ adam-cli/src/test/scala/org/bdgenomics/adam/cli/AboutSuite.scala
@@ -25,7 +25,6 @@ class AboutSuite extends FunSuite {
   test("template variables have been replaced") {
     assert(about.artifactId !== "${project.artifactId}")
     assert(about.buildTimestamp !== "${maven.build.timestamp}")
-    assert(about.commit !== "${git.commit.id}")
     assert(about.hadoopVersion !== "${hadoop.version}")
     assert(about.scalaVersion !== "${scala.version}")
     assert(about.sparkVersion !== "${spark.version}")
@@ -35,7 +34,6 @@ class AboutSuite extends FunSuite {
   test("templated values are not empty") {
     assert(about.artifactId.nonEmpty)
     assert(about.buildTimestamp.nonEmpty)
-    assert(about.commit.nonEmpty)
     assert(about.hadoopVersion.nonEmpty)
     assert(about.scalaVersion.nonEmpty)
     assert(about.sparkVersion.nonEmpty)
