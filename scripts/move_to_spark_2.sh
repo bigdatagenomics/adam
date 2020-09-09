bac1cb1984a0d3553d9a9f567774ec58f4dee415
Michael L Heuer <heuermh@acm.org>
2020-09-09 16:04:46 -0500

    Update Spark dependency version to 3.0.1 (#2274)
    * Update Spark dependency version to 3.0.1.
    
    * Update jenkins build for Spark 3.0.1.

--- scripts/move_to_spark_2.sh
+++ scripts/move_to_spark_2.sh
@@ -19,5 +19,5 @@ find . -name "pom.xml" -exec sed \
     -e "/adam-/ $substitution_cmd" \
     -e "/utils-/ s/-spark3_2\.1/-spark2_2\.1/" \
     -e "/utils-/ $substitution_cmd" \
-    -e "/spark.version/ s/3.0.0/2.4.6/g" \
+    -e "/spark.version/ s/3.0.1/2.4.6/g" \
     -i.spark2.bak '{}' \;
